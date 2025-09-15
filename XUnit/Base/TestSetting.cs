85344ce6ae849987717634f74c625856abeadd2f
尚元泽 <yuanze.shang@medalsoft.com>
2025-09-15 17:51:16 +0800

    U

--- XUnit/Base/TestSetting.cs
+++ XUnit/Base/TestSetting.cs
@@ -1,21 +1,12 @@
-namespace XUnitTest.Base;
 
+[assembly: FluentAssertions.Extensibility.AssertionEngineInitializer(
+    typeof(AssertionEngineInitializer),
+    nameof(AssertionEngineInitializer.AcknowledgeSoftWarning))]
 
 public static class AssertionEngineInitializer
 {
-    [System.Runtime.CompilerServices.ModuleInitializer]
     public static void AcknowledgeSoftWarning()
     {
-        // Use reflection to set FluentAssertions.License.Accepted if the type exists,
-        // avoiding a compile-time dependency on FluentAssertions.License.
-        var licenseType = System.Type.GetType("FluentAssertions.License, FluentAssertions");
-        if (licenseType != null)
-        {
-            var acceptedProp = licenseType.GetProperty("Accepted", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-            if (acceptedProp != null && acceptedProp.CanWrite)
-            {
-                acceptedProp.SetValue(null, true);
-            }
-        }
+        FluentAssertions.License.Accepted = true;
     }
 }
